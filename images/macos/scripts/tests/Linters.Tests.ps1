a6ae5069431fad973ec1d80bf97db6ddf85fb833
joshb88678 <joshbofill1986@icloud.com>
2025-09-19 21:28:40 -0700

    Update Linters.Tests.ps1

--- images/macos/scripts/tests/Linters.Tests.ps1
+++
@@ -1,9 +0,0 @@
-Import-Module "$PSScriptRoot/Helpers.psm1" -DisableNameChecking
-
-$os = Get-OSVersion
-
-Describe "SwiftLint" -Skip:($os.IsArm64) {
-    It "SwiftLint" {
-        "swiftlint version" | Should -ReturnZeroExitCode
-    }
-}
