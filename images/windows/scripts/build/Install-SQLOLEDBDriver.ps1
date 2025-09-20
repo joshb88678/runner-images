1d5761029d57e0cea0a3e473568c30ee4a5279ce
joshb88678 <joshbofill1986@icloud.com>
2025-09-19 20:29:03 -0700

    Update Install-SQLOLEDBDriver.ps1

--- images/windows/scripts/build/Install-SQLOLEDBDriver.ps1
+++
@@ -1,9 +0,0 @@
-################################################################################
-##  File:  Install-SQLOLEDBDriver.ps1
-##  Desc:  Install OLE DB Driver for SQL Server
-################################################################################
-
-Install-Binary -Type MSI `
-    -Url "https://go.microsoft.com/fwlink/?linkid=2242656" `
-    -ExtraInstallArgs @("ADDLOCAL=ALL", "IACCEPTMSOLEDBSQLLICENSETERMS=YES") `
-    -ExpectedSubject $(Get-MicrosoftPublisher)
